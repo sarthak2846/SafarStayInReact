57acdfcf1e6c930b0f58c32addb4549953a76c43
Sarthak Singh <sarthaksingh54682@gmail.com>
2026-07-11 20:30:37 +0530

    Enable email sending with Nodemailer

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -38,14 +38,8 @@ export const signUp = async (req, res) => {
       text: `Your OTP is ${otp}. It will expire in 5 minutes. Verify your account using this email.`,
     };
 
-    // await transporter.sendMail(mailOptions);
-  transporter.verify(function (error, success) {
-  if (error) {
-    console.log("Nodemailer configuration error:", error);
-  } else {
-    console.log("Server is ready to take our messages");
-  }
-});
+     await transporter.sendMail(mailOptions);
+ 
 
     
     const verifyToken = jwt.sign(
